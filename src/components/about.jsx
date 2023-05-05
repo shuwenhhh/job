ca7b0a3ca5b290daf788ef2240830ac5038a9c3e
cc <chengcheng0441@gmail.com>
2023-05-04 21:40:44 -0400

    cc-commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,13 +5,10 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
-          </div>
+
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
+              <h2>Ready to get started?</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>Why Choose Us?</h3>
               <div className="list-style">
